e68045e2c9ae688eabe91cefdad22d4dc147030b
Adam Hunter <hunteradam13@gmail.com>
2021-10-26 14:02:34 -0600

    adding h1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const Rollbar = require('rollbar')
 
 let rollbar = new Rollbar({
-    accessToken: '3c1ca1379d7e4a079023a8ba3967124a',
+    accessToken: '4c134f81c71f41c58be7a1bbebf2bd93',
     captureUncaught: true,
     captureUnhandledRejections: true
 })
@@ -20,26 +20,26 @@ app.get('/style', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/styles.css'))
 })
 
-let students =[]
+// let students =[]
 
-app.post('/api/student', (req, res) => {
-    let {name} = req.body
-    name = name.trim()
+// app.post('/api/student', (req, res) => {
+//     let {name} = req.body
+//     name = name.trim()
 
-    const index = students.findIndex(studentName => studentName === name)
+//     const index = students.findIndex(studentName => studentName === name)
 
-    if(index === -1 && name !== ''){
-        students.push(name)
-        rollbar.log('Student added successfully', {author: 'Adam', type: 'manual entry'})
-        res.status(200).send(students)
-    } else if(name === '') {
-        rollbar.error('No name given')
-        res.status(400).send('Must provide a name.')
-    } else{
-        rollbar.critical('Student already exists.')
-        res.status(400).send('that student already exists')
-    }
-})
+//     if(index === -1 && name !== ''){
+//         students.push(name)
+//         rollbar.log('Student added successfully', {author: 'Adam', type: 'manual entry'})
+//         res.status(200).send(students)
+//     } else if(name === '') {
+//         rollbar.error('No name given')
+//         res.status(400).send('Must provide a name.')
+//     } else{
+//         rollbar.critical('Student already exists.')
+//         res.status(400).send('that student already exists')
+//     }
+// })
 
 app.use(rollbar.errorHandler())
 
